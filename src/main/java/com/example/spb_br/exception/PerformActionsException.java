17da3ec392936f10a857c3485d81903f0554fc6d
Dianana101 <irmarude1984@gmail.com>
2024-08-27 12:18:29 +0300

    (*): added http-code

--- src/main/java/com/example/spb_br/exception/PerformActionsException.java
+++ src/main/java/com/example/spb_br/exception/PerformActionsException.java
@@ -1,5 +1,9 @@
 package com.example.spb_br.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class PerformActionsException extends RuntimeException {
     public PerformActionsException(String msg) {
         super(msg);
